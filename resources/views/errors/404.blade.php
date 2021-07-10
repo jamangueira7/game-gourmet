51b94bdfe485bcdf23777149116ec60b0661e438
João Mangueira <jamjs7@gmail.com>
2021-07-10 18:37:25 -0300

    alternado 404

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -15,9 +15,9 @@
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" />
 
     </head>
-    <body style="background-color: #005cbf;">
-    <div style="display: flex;justify-content: center;align-items: center; min-height: 100px; font-size: 100px; font-weight: bold;">
-        <span>Está perdido?</span>
+    <body style="background-color: #0565cb;">
+    <div style="display: flex;justify-content: center;align-items: center; min-height: 100px; font-size: 100px; font-weight: bold; color: #fff;">
+        <span>ESTÁ PERDIDO?</span>
     </div>
     <div style="background-image: url({{ asset('/img/404.jpg') }}); background-repeat: no-repeat;background-size: cover; min-height: 800px;"></div>
 
